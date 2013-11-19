5f40762e218c622ea4fa678e651bba8ec1599d34
Chris Gindel <cgindel@rethinkrobotics.com>
2013-11-19 09:27:30 -0500

    fix type in spoof

--- baxter_spoof.sh
+++ baxter_spoof.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 #spoof sdk enable and gripper state
 
-rostopic pub /robot/state baxter_msgs/AssemblyState "{enabled: true, stopped: false, error: false, estop_button: 0, estop_source: 0}" &
+rostopic pub /robot/state baxter_core_msgs/AssemblyState "{enabled: true, stopped: false, error: false, estop_button: 0, estop_source: 0}" &
 
 rostopic pub /robot/end_effector/left_gripper/state baxter_core_msgs/EndEffectorState "{timestamp: {secs: 0, nsecs: 0}, id: 1, enabled: 1, calibrated: 1, ready: 1, moving: 0, gripping: 0, missed: 0, error: 0, command: 0, position: 0.0, force: 0.0, state: '{"error_flags" : 0, "op_mode": 2}', command: 'no_op', command_sender: '', command_sequence: 0}" &
 
