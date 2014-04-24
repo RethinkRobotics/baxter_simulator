db2f184b7632d652a7652b1742eaca505fd9c653
rethink-hmalaichamee <hmalaichamee@rethinkrobotics.com>
2014-04-24 11:31:43 -0400

    The laserscan topic name was modified to be in the sim namespace from robot's namespace

--- baxter_sim_hardware/src/baxter_emulator.cpp
+++ baxter_sim_hardware/src/baxter_emulator.cpp
@@ -54,9 +54,9 @@ static const std::string BAXTER_RIGHT_GRIPPER_PROP =
     "robot/end_effector/right_gripper/properties";
 static const std::string BAXTER_JOINT_TOPIC = "robot/joint_states";
 static const std::string BAXTER_LEFT_LASER_TOPIC =
-    "robot/laserscan/left_hand_range/state";
+    "sim/laserscan/left_hand_range/state";
 static const std::string BAXTER_RIGHT_LASER_TOPIC =
-    "robot/laserscan/right_hand_range/state";
+    "sim/laserscan/right_hand_range/state";
 static const std::string BAXTER_LEFT_IR_TOPIC = "robot/range/left_hand_range";
 static const std::string BAXTER_RIGHT_IR_TOPIC = "robot/range/right_hand_range";
 static const std::string BAXTER_LEFT_IR_STATE_TOPIC =
