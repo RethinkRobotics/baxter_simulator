bd08e382e25e47133b4e7da34052b31317abb753
Ian McMahon <imcmahon@rethinkrobotics.com>
2015-01-06 15:08:07 -0500

    Updated licenses to 2015

--- baxter_sim_hardware/src/baxter_emulator.cpp
+++ baxter_sim_hardware/src/baxter_emulator.cpp
@@ -1,5 +1,5 @@
 /*********************************************************************
- # Copyright (c) 2014, Rethink Robotics
+ # Copyright (c) 2015, Rethink Robotics
  # All rights reserved.
  #
  # Redistribution and use in source and binary forms, with or without
